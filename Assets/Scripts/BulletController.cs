2aba8f53f5f04d812650580a30dcb2c9b8ad8b07
H143UY <145687297+H143UY@users.noreply.github.com>
2023-09-27 19:41:00 +0700

    lesson1

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed = 6;
+    private float time = 0;
+
+    void Update()
+    {
+        if(time == 800)
+        {
+            Destroy(this.gameObject);
+        }
+        time++;
+        this.transform.position += transform.up * Time.deltaTime * speed; 
+    }
+}
---
+++ Assets/Scripts/bulletcontroller2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletcontroller2 : MonoBehaviour
+{
+    public float speed = 5;
+    private float time = 0;
+  
+    void Update()
+    {
+        if (time == 300)
+        {
+            Destroy(this.gameObject);
+        }
+        time++;
+        this.transform.position += transform.up * Time.deltaTime * speed;
+    }
+}
