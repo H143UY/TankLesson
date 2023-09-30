8fd8e1d01fd7d6e007a9c012731fcee4950797ae
H143UY <145687297+H143UY@users.noreply.github.com>
2023-09-30 09:31:12 +0700

    btvnlesson 2

---
+++ Assets/Scripts/RotateGun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGun : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject transhot1;
+    public GameObject transhot2;
+    public GameObject transhot3;
+    public GameObject transhot4;
+    void Update()
+    {
+        this.gameObject.transform.Rotate(0, 0, 5);
+        Instantiate(bullet, transhot1.transform.position, transhot1.transform.rotation);
+        Instantiate(bullet, transhot2.transform.position, transhot2.transform.rotation);
+        Instantiate(bullet, transhot3.transform.position, transhot3.transform.rotation);
+        Instantiate(bullet, transhot4.transform.position, transhot4.transform.rotation);
+        
+    }
+}
--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -4,35 +4,67 @@ using UnityEngine;
 
 public class enemyController : MonoBehaviour
 {
-    public int speed;
+    public float speed;
     public GameObject gun;
     public GameObject transhoot;
     public GameObject bullet;
     public GameObject Player;
+
+    public float timedelay;
+    public float timeBullet;
+
+    public int count;
+    public GameObject USung;
+
     void Start()
     {
-        
+        timedelay = timeBullet;
     }
     void Update()
+    { 
+        Movement();
+        fireBullet();
+    }
+    void fireBullet()
+    {
+        timedelay -= Time.deltaTime;
+        if (timedelay <= 0)
+        {
+            Instantiate(bullet, transform.position, transhoot.transform.rotation);
+            timedelay = timeBullet;
+            Debug.Log(speed + "/enemy dung lai");
+            speed = 0;
+            Invoke("StopMoverment", 5f);
+        }
+
+    }
+
+    void Movement()
     {
         Vector3 direction = Player.transform.position;
         var gunDirection = direction - transform.position;
-        if(gunDirection != Vector3.zero)
+        if (gunDirection != Vector3.zero)
         {
             this.gameObject.transform.up = gunDirection;
         }
         this.gameObject.transform.position += gunDirection * Time.deltaTime * speed;
 
         gun.transform.up = gunDirection;
-        if(Random.Range(0, 100) % 50 == 0 )
-            Instantiate(bullet,transform.position,transhoot.transform.rotation);
-        }
-    private void OnTriggerEnter2D(Collider2D collision) // collision : th va cham voi minh
+    }
+    void OnTriggerEnter2D(Collider2D collision) // collision : th va cham voi minh
     {
-        if(collision.transform.tag == "bullet") 
+        if (collision.transform.tag == "bullet")
+            count++;
+        if (count == 10)
         {
             Destroy(this.gameObject);
+            Instantiate(USung, transform.position, transform.rotation);
         }
+    }
+
+    void StopMoverment()
+    {
+        speed = 1;
     }
 }
 
